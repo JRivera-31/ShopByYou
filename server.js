0e98c1d2bd81060331621a4767454aaf6c58c1dd
George Flores <georgeflores886@gmail.com>
2020-06-13 18:36:26 -0400

    added controller route and handlebar package

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
+//express
 var express = require("express");
-
-
 var app = express();
+//port
 var PORT = process.env.PORT || 8080;
-
-
+// app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,9 +14,9 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 
 app.set("view engine", "handlebars");
-
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+//routes
+var routes = require("./controllers/");
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
