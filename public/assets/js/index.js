10a28e9c8b9a4546dee5fe63b6298b310f332e0b
George Flores <georgeflores886@gmail.com>
2020-06-15 18:56:20 -0400

    added New item object

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,14 @@
-// Test
+//inputting item up for sell
+$(function () {
+    $(".ui inverted").on("click", event => {
+        event.preventDefault();
+        let newItem = {
+            item_name: $('item').val().trim(),
+            price: $('price').val().trim(),
+            category: $('category').val().trim(),
+            quantity: $('quantity').val().trim(),
+            description: $('description').val().trim()
+        };
+        
+    })
+})
