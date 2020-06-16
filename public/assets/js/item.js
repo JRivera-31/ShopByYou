b2af005e3b1769c5c122c8143865a2993a8a0351
George Flores <georgeflores886@gmail.com>
2020-06-16 11:59:33 -0400

    added localstorage for cart

--- public/assets/js/item.js
+++ public/assets/js/item.js
@@ -1,5 +1,7 @@
+const { json } = require("sequelize");
+
 //inputting item up for sell
-$(document).ready(function() {
+$(function() {
     let item = {
         //grabbing name values
         item_name: $('.item').val().trim(),
@@ -21,8 +23,14 @@ $(document).ready(function() {
     });
     //add item to cart
     $(".ui inverted purple button").on("click", event =>{
+        localStorage.set("userCart", JSON.stringify(item));
+        let userItems = localStorage.getItem("userCart");
 
+        
     })
+    //
+
+
     //remove from favorites
     $(".ui inverted red button").on("click", event => {
         let id = $(this).data("id");
@@ -34,6 +42,5 @@ $(document).ready(function() {
         })
     });
     //remove from cart
-
 })
    
