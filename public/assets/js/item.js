bd9251e48187163b447c3deb5b3dddf8762cda25
George Flores <georgeflores886@gmail.com>
2020-06-16 10:56:08 -0400

    added classes in sell handlebars and updated item.js

--- public/assets/js/item.js
+++ public/assets/js/item.js
@@ -1,24 +1,28 @@
 //inputting item up for sell
 $(document).ready(function() {
+    let item = {
+        //grabbing name values
+        item_name: $('.item').val().trim(),
+        price: $('.price').val().trim(),
+        category: $('.category').val().trim(),
+        quantity: $('.quantity').val().trim(),
+        description: $('.description').val().trim()
+    };
+    //adding item to sell in sell.handlbars
     $("#itemForm").on("click", event => {
         event.preventDefault();
-        let newItem = {
-            //grabbing name values
-            item_name: $('item').val().trim(),
-            price: $('price').val().trim(),
-            category: $('category').val().trim(),
-            quantity: $('quantity').val().trim(),
-            description: $('description').val().trim()
-        };
         $.ajax('/api/item', {
             type: 'POST',
-            data: newItem
+            data: item
         }).then(function() {
             console.log('Added item to database');
             location.reload();
         });
     });
-    $(".")
+    //add item to cart
+    $(".ui inverted purple button").on("click", event =>{
+
+    })
     //remove from favorites
     $(".ui inverted red button").on("click", event => {
         let id = $(this).data("id");
