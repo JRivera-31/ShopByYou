0724ab8f8c2f6da830a0c5ff948876c7c05289fe
George Flores <georgeflores886@gmail.com>
2020-06-17 20:55:30 -0400

    accepting merges

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -20,74 +20,6 @@ module.exports = function (app) {
     res.json(req.user);
   });
 
-<<<<<<< HEAD
-    app.get("/api/user_data", (req, res) => {
-        if(!req.user){
-            res.json({});
-        } else {
-            res.json({
-                email: req.user.email,
-                id: req.user.id
-            });
-        }
-    });
-
-    //get all items
-    app.get("/api/items", function(req, res) {
-        db.Item.findAll({}).then(function(dbItem) {
-            res.json(dbItem);
-        });
-    });
-    //get all items from specific categories
-    app.get("api/categories", function(req, res) {
-        db.Item.findAll({
-            where: {
-                category: req.params.category
-            }
-        }).then(function(dbItem) {
-            res.json(dbItem);
-        });
-    })
-
-    //adding item to sell in app
-    app.post("/api/items", (req, res) => {
-        db.Item.create({
-            item_name: req.body.item_name,
-            category: req.body.category,
-            quantity: req.body.quantity,
-            //placeholder for image
-            description: req.body.description,
-            price: req.body.price
-        }).then(function (dbItem) {
-            res.json(dbItem);
-        }).catch(function(err) {
-            res.status(401).json(err);
-        })
-    });
-    //move item to cart
-    // app.put("/api/items", function(req, res) {
-    //     db.Item.updateOne({
-    //         item_name: req.body.item_name,
-    //         quantity: req.body.quantity,
-    //         //placeholder for image
-    //         price: req.body.price
-    //     }).then(function (dbItem) {
-    //         res.json(dbItem);
-    //     }).catch(function(err) {
-    //         res.status(401).json(err);
-    //     });
-    // });
-
-    //remove item from cart
-    app.delete("/api/items/:id", function(req, res) {
-        db.Item.destroy({
-            where: {id: req.params.id}
-        }).then(function(dbItem) {
-            res.json(dbItem);
-        });
-    });
-}
-=======
   app.post("/api/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
@@ -129,4 +61,3 @@ module.exports = function (app) {
   //     res.json(req.files)
   // })
 };
->>>>>>> 2482869a9f4e608402e5078afda3856049ad3aae
