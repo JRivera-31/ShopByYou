d9324cd2c91428d8b57b76ab3dcb7de3a5013535
Josh R <jriver35@uncc.edu>
2020-06-18 17:00:30 -0400

    Delete functionality working

--- public/assets/js/item.js
+++ public/assets/js/item.js
@@ -13,7 +13,6 @@ $(function () {
         const itemName = $('.itemName').val().trim();
         const price = $('.price').val().trim();
         const category = $('#categories').val();
-        const quantity = $('.quantity').val().trim();
         const description = $('.description').val().trim();
         const file = $("#userImg")[0].files[0]
         console.log(file)
@@ -23,16 +22,10 @@ $(function () {
         formData.append('file', $("#userImg")[0].files[0])
         console.log('here')
 
-        // Validate if price is not a number
-        // if(typeof price != "number") {
-        //     return alert("Price must be a number!");
-        // }
-
         // Create object for values
         let userItem = {
             item_name: itemName,
             category: category,
-            quantity: quantity,
             description: description,
             price: price,
         }
