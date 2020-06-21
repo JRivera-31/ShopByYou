b344c350a7c99c80c48cfc65ae2a182a3f12cf7e
Josh R <jriver35@uncc.edu>
2020-06-21 19:58:58 -0400

    Added multiple items in cart functionality, need to clean up.

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -4,25 +4,29 @@ const totalPrice = $("#totalPrice")
 const clearCart = $("#clearCart")
 
 // Get items from local storage
-let cartItem = JSON.parse(localStorage.getItem("cart-items"))
+// let cartItem = JSON.parse(localStorage.getItem("cart-items"))
 
-console.log(cartItem)
+// console.log(cartItem)
 
-// Create html
-let displayImg = `<img src=${cartItem.image} class=ui right spaced image />`
+// Get array of items from local storage
+var cartItems = JSON.parse(localStorage.getItem("cart-items")) || [];
 
-let displayItem = `<p class='header'><strong>Item Name: </strong>${cartItem.name}</p>
-<div class="meta">
- <span class="price"><strong>Price: </strong>${cartItem.price}</span>
-</div>`
-  
-let price = `<p>$${cartItem.price}</p>`
+for (let i = 0; i < cartItems.length; i++) {
+  // Create html
+  let displayImg = `<img src=${cartItems[i].image} class=ui right spaced image />`
 
-// Append elements
-cartArea.append(displayItem)
-cartArea.append(displayImg)
-totalPrice.append(price)
+  let displayItem = `<p class='header'><strong>Item Name: </strong>${cartItems[i].name}</p>
+  <div class="meta">
+  <span class="price"><strong>Price: </strong>${cartItems[i].price}</span>
+  </div>`
+    
+  let price = `<p>$${cartItems[i].price}</p>`
 
+  // Append elements
+  cartArea.append(displayItem)
+  cartArea.append(displayImg)
+  totalPrice.append(price)
+}
 // To clear cart
 clearCart.on("click", function(event) {
   localStorage.clear()
--- public/assets/js/shop.js
+++ public/assets/js/shop.js
@@ -1,38 +1,12 @@
-$(".purchasebutton").on("click", function (event) {
-    const name = $(this).data("name")
-    const price = $(this).data("price")
-    const img = $(this).data("img")
-    console.log(price)
-
-    let cartItem = {name: name, price: price, image: img}
-
-    localStorage.setItem("cart-items", JSON.stringify(cartItem))
-    let id = $(this).data("id")
-
-    $.ajax({
-            type: "delete",
-            url: "/api/deleteitem/" + id
-        }).then(function(deletedItem) {
-            console.log(console.log(deletedItem))
-            window.location.reload()
-            return alert("Item added to cart!")
-        }).catch(function(err) {
-          console.log(err)
-        }) 
-})
-
-// var cartItems = JSON.parse(localStorage.getItem("cart-items")) || [];
-
 // $(".purchasebutton").on("click", function (event) {
 //     const name = $(this).data("name")
 //     const price = $(this).data("price")
 //     const img = $(this).data("img")
 //     console.log(price)
 
-//     let item = {name: name, price: price, image: img}
-//     cartItems.push(item);
+//     let cartItem = {name: name, price: price, image: img}
 
-//     localStorage.setItem("cart-items", JSON.stringify(cartItems))
+//     localStorage.setItem("cart-items", JSON.stringify(cartItem))
 //     let id = $(this).data("id")
 
 //     $.ajax({
@@ -46,3 +20,28 @@ $(".purchasebutton").on("click", function (event) {
 //           console.log(err)
 //         }) 
 // })
+let cartItems = []
+
+$(".purchasebutton").on("click", function (event) {
+    const name = $(this).data("name")
+    const price = $(this).data("price")
+    const img = $(this).data("img")
+
+    let item = {name: name, price: price, image: img}
+    cartItems.push(item);
+    console.log(cartItems)
+
+    localStorage.setItem("cart-items", JSON.stringify(cartItems))
+    let id = $(this).data("id")
+
+//     $.ajax({
+//             type: "delete",
+//             url: "/api/deleteitem/" + id
+//         }).then(function(deletedItem) {
+//             console.log(console.log(deletedItem))
+//             window.location.reload()
+//             return alert("Item added to cart!")
+//         }).catch(function(err) {
+//           console.log(err)
+//         }) 
+})
