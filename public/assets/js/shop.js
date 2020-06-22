ed168f23a5c9c3fc3d3721a43f2cac9b89847bae
Josh R <jriver35@uncc.edu>
2020-06-21 20:23:20 -0400

    Added added to cart alert

--- public/assets/js/shop.js
+++ public/assets/js/shop.js
@@ -10,7 +10,8 @@ $(".purchasebutton").on("click", function (event) {
   console.log(cartItems);
 
   localStorage.setItem("cart-items", JSON.stringify(cartItems));
-  let id = $(this).data("id");
+ 
+  return alert("Item added to cart!")
 
   // $.ajax({
   //   type: "delete",
